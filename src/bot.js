c4bfb2747907d06692f9e38eed57af45c6240e9e
Bileq <dominik.bil64@gmail.com>
2021-11-16 19:10:09 +0100

    Setup commands for future adding

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,46 @@
 require("dotenv").config();
 
 const { Client, Intents } = require("discord.js");
+const Discord = require("discord.js");
 const bot = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 const PREFIX = "!";
+
+bot.commands = new Discord.Collection
+
+//collection of commands
+const fs = require('fs');
+
+const commandsFiles = fs.readdirSync('src/commands/')
+   .filter(file => file.endsWith(".js"));
+
+
+for (const file of commandsFiles) {
+   const commandFile = require(`./commands/${file}`);
+   bot.commands.set(commandFile.name, commandFile);
+   console.log(bot.commands)
+}
 
 //commands handling
 bot.on("messageCreate", msg => {
    if (msg.author.bot) return;
    if (msg.content.startsWith(PREFIX)) {
       const [command, ...args] = msg.content
-        .trim()
-        .substring(PREFIX.length)
-        // "+" in this regExp concats all spaces
-        .split(/\s+/);
-      console.log(command);
+         .trim()
+         .substring(PREFIX.length)
+         .toLowerCase()
+         .split(/\s+/);
+
+      switch (command) {
+         case "play":
+            bot.commands.get("play").execute(msg, args);
+            break;
+         default:
+            bot.commands.get("notFound").execute(msg, args);
+      }
    }
 });
-
 
 bot.on("ready", () => {
    console.log(`${bot.user.tag} is up`);
